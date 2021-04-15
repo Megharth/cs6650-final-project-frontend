52160c02ee1eb5fa6fcd51014a190f5dceb5712d
Megharth <m.c.lakhataria@gmail.com>
2021-04-15 10:11:46 -0400

    add register button

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ const Login = () => {
       <TextField variant="outlined" label="email" fullWidth/>
       <TextField variant="outlined" label="password" type="password" fullWidth/>
       <Button variant="contained" size="large" color="primary">Login</Button>
+      <Button variant="contained" size="large" color="secondary">Register</Button>
     </div>
   )
 } 
