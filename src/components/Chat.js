75f94566762c07313cfcad4f5d2f0df9e80628b5
Megharth <m.c.lakhataria@gmail.com>
2021-04-16 10:47:11 -0400

    add reactive components

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,163 +4,132 @@ import {Grid,
     ListItemText, 
     ListItemIcon, 
     Divider,
-    Card,
-    CardContent,
     TextField,
-    Icon
 } from '@material-ui/core';
+import {io} from 'socket.io-client';
 
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import SendIcon from '@material-ui/icons/Send';
-
 import '../css/Chat.css';
+import { useEffect, useState } from 'react';
 
 const Chat = () => {
+    const [users, setUsers] = useState({});
+    const [selectedUser, setSelectedUser] = useState('');
+    const [chats, setChats] = useState({});
+    const [input, setInput] = useState('');
+
+    useEffect(() => {
+        const socket = io(process.env.REACT_APP_SOCKET_URL, { autoConnect: false });
+        socket.auth = {email: window.localStorage.getItem('user')};
+        socket.connect();
+
+
+        socket.on("connect_error", (err) => {
+            console.log(err.message)
+        });
+
+        socket.on("users", (newUsers) => {
+            delete newUsers[window.localStorage.getItem('user')];
+            setUsers(newUsers);
+        });
+
+        socket.on("new connection", (user) => {
+            setUsers(prevUsers => {
+                prevUsers[user.email] = user.id;
+                return Object.assign({}, prevUsers);
+            });
+        });
+
+        socket.on("user disconnected", (email) => {
+            setUsers(prevUsers => {
+                delete prevUsers[email];
+                return Object.assign({}, prevUsers);
+            });
+        });
+
+        return () => socket.disconnect();
+    }, []);
+
+    const createList = (emails) => {
+        return emails.length > 0 ? (
+            <List>
+                {
+                    emails.map(user => (
+                        <div key={user}>
+                            <ListItem button onClick={() => setSelectedUser(user)}>
+                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
+                                <ListItemText primary={user} secondary="Some message" />
+                            </ListItem>
+                            <Divider />
+                        </div>
+                    ))
+                }
+            </List>
+        ) : (
+            <div className="no-user">No users online</div>
+        );
+    }
+
+    const renderMessages = () => {
+        return chats[selectedUser] ? chats[selectedUser].map(msg => (
+            <div className="message">
+                <span className={msg.sender === selectedUser ? 'left-message' : 'right-message'}>Message 1</span>
+            </div>
+        )) : (
+            <div className="no-user">Type a message to start chatting</div>
+        );
+    }
+
+    const sendMessage = () => {
+        console.log(input);
+        setInput('');
+    }
+
     return(
         <div>
             <Grid container spacing={0}>
                 <Grid item sm={3} xs={12}>
-                    <div id="left-pane">
-                        <List>
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                            <ListItem button>
-                                <ListItemIcon><AccountCircleIcon fontSize="large" /></ListItemIcon>
-                                <ListItemText primary="User 1" secondary="Some message" />
-                            </ListItem>
-                            <Divider />
-                        </List>
+                    <div id="left-pane">                        
+                        {createList(Object.keys(users))}
                     </div>
                 </Grid>
                 <Grid item sm={9} xs={12}>
-                    <div id="right-pane">
-                        <div className="header-container">
-                            <span className="header-username">User 1</span>
-                        </div>
-                        <div className="message-container">
-                            <div className="message">
-                                <span className="left-message">Message 1</span>
+                    {selectedUser ? (
+                        <div id="right-pane">
+                            <div className="header-container">
+                                <span className="header-username">{selectedUser}</span>
                             </div>
-                            <div className="message">
-                                <span className="right-message">Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="left-message">Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="left-message">Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="right-message">Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="left-message">Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="left-message">Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="right-message">Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="left-message">Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="right-message">Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="left-message">Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="right-message">Message 1</span>
-                            </div>
-                            <div className="message">
-                                <span className="left-message">Message 1</span>
-                            </div>
-                        </div>
                         
-                        <Grid container className="input-container">
-                            <Grid item xs={11}>
-                                <TextField 
-                                    className="message-input"
-                                    fullWidth
-                                    // onKeyUp={(e) => { setEmail(e.target.value); }}
-                                    variant="outlined" 
-                                    placeholder="Type your Message"
-                                />
+                            <div className="message-container">
+                                {renderMessages()}
+                            </div>
+                            
+                            <Grid container className="input-container">
+                                <Grid item xs={11}>
+                                    <TextField 
+                                        className="message-input"
+                                        fullWidth
+                                        onChange={(e) => setInput(e.target.value)}
+                                        onKeyUp={(e) => { if(e.key === 'Enter') sendMessage(); }}
+                                        variant="outlined" 
+                                        value={input}
+                                        placeholder="Type your Message"
+                                    />
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <SendIcon color="action" className="send-icon" onClick={sendMessage} />
+                                </Grid>
                             </Grid>
-                            <Grid item xs={1}>
-                                <SendIcon color="action" className="send-icon" />
-                            </Grid>
-                        </Grid>
-                    </div>
+                        </div>
+                    ) : (
+                        <div className="no-user">Please select a chat to see messages</div>
+                    )}
+
                 </Grid>
             </Grid>
         </div>
-    )
+    );
 }
 
 export default Chat;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-
+import {useHistory} from 'react-router-dom';
 import '../css/Login.css';
 import {TextField, Button, Snackbar} from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
@@ -11,60 +11,64 @@ const Login = () => {
   const [toastMsg, setToastMsg] = useState(false);
   const [toastStatus, setToastStatus] = useState(false);
   const errorCodes = new Set([401, 403, 404]);
-  const API = 'http://localhost:8000/';
+  const history = useHistory();
 
   const Alert = (props) => (<MuiAlert elevation={6} variant="filled" {...props} />);
 
   const login = async () => {
-    const response = await fetch(API + 'login', {
-      method: 'POST',
-      body: JSON.stringify({email, password}),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const body = await response.json();
-    setToastMsg(body.message);
-    setToastStatus(body.status);
-    setShowToast(true);
+	const response = await fetch(process.env.REACT_APP_API_URL + 'login', {
+	  method: 'POST',
+	  body: JSON.stringify({email, password}),
+	  headers: {
+		'Content-Type': 'application/json'
+	  }
+	});
+	const body = await response.json();
+	setToastMsg(body.message);
+	setToastStatus(body.status);
+	setShowToast(true);
+	if(!errorCodes.has(body.status)){
+		history.push('/chat');
+		window.localStorage.setItem('user', email);
+	}
   }
 
   const register = async () => {
-    const response = await fetch(API + 'register', {
-      method: 'POST',
-      body: JSON.stringify({email, password}),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const body = await response.json();
-    setToastMsg(body.message);
-    setToastStatus(body.status);
-    setShowToast(true);
+	const response = await fetch(process.env.REACT_APP_API_URL + 'register', {
+	  method: 'POST',
+	  body: JSON.stringify({email, password}),
+	  headers: {
+		'Content-Type': 'application/json'
+	  }
+	});
+	const body = await response.json();
+	setToastMsg(body.message);
+	setToastStatus(body.status);
+	setShowToast(true);
   }
   return(
-    <div>
-      <div id="login">
-        <TextField 
-          onKeyUp={(e) => { setEmail(e.target.value); }}
-          variant="outlined" 
-          label="email" 
-          fullWidth />
-        <TextField 
-          onKeyUp={(e) => { setPassword(e.target.value); }}
-          variant="outlined" 
-          label="password" 
-          type="password" 
-          fullWidth/>
-        <Button variant="contained" size="large" color="primary" onClick={login}>Login</Button>
-        <Button variant="contained" size="large" color="secondary" onClick={register}>Register</Button>
-      </div>
-      <Snackbar open={showToast} autoHideDuration={3000} onClose={() => setShowToast(false)}>
-        <Alert severity={errorCodes.has(toastStatus) ? 'error' : 'success'} onClose={() => setShowToast(false)}>
-          {toastMsg}
-        </Alert>
-      </Snackbar>
-    </div>
+	<div>
+	  <div id="login">
+		<TextField 
+		  onKeyUp={(e) => { setEmail(e.target.value); }}
+		  variant="outlined" 
+		  label="email" 
+		  fullWidth />
+		<TextField 
+		  onKeyUp={(e) => { setPassword(e.target.value); }}
+		  variant="outlined" 
+		  label="password" 
+		  type="password" 
+		  fullWidth/>
+		<Button variant="contained" size="large" color="primary" onClick={login}>Login</Button>
+		<Button variant="contained" size="large" color="secondary" onClick={register}>Register</Button>
+	  </div>
+	  <Snackbar open={showToast} autoHideDuration={3000} onClose={() => setShowToast(false)}>
+		<Alert severity={errorCodes.has(toastStatus) ? 'error' : 'success'} onClose={() => setShowToast(false)}>
+		  {toastMsg}
+		</Alert>
+	  </Snackbar>
+	</div>
   )
 } 
 export default Login;
