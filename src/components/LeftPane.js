0ad0205372f0c87defa318335578529ea6a8468e
Megharth <m.c.lakhataria@gmail.com>
2021-04-18 06:45:38 -0400

    add logout functionality

--- src/components/LeftPane.js
+++ src/components/LeftPane.js
@@ -4,14 +4,22 @@ import {Grid,
     ListItemText, 
     ListItemIcon, 
     Divider,
-    // TextField,
+    Popper,
+    TextField,
+    Card,
+    Button,
 } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 
 const LeftPane = ({users, setSelectedUser, chats}) => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const history = useHistory(); 
+
     const createList = (emails) => {
         return emails.length > 0 ? (
             <List>
@@ -32,14 +40,36 @@ const LeftPane = ({users, setSelectedUser, chats}) => {
         );
     }
 
+    const logout = () => {
+        history.push('/');
+        window.localStorage.setItem('user', null);
+    }
     return(
         <Grid id="left-pane" item sm={3} xs={12}>
             <div className="chat-list">                        
                 {createList(Object.keys(users))}
             </div>
             <div className="actions">
-                <PersonOutlineOutlinedIcon fontSize="large" className="action" />
-                <PowerSettingsNewIcon fontSize="large" className="action" />
+                <PersonOutlineOutlinedIcon 
+                    aria-describedby="account-menu" 
+                    onClick={(e) => { anchorEl? setAnchorEl(null) : setAnchorEl(e.currentTarget); }} 
+                    fontSize="large" 
+                    className="action" />
+                <PowerSettingsNewIcon 
+                    fontSize="large" 
+                    className="action" 
+                    onClick={logout} />
+                <Popper
+                    id="account-menu"
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                >
+                    <Card className="account-menu">
+                        <TextField label="Email" variant="outlined" disabled value={window.localStorage.getItem('user')}></TextField>
+                        <TextField label="Name" variant="outlined" placeholder="Enter your name"></TextField>
+                        <Button color="primary" variant="outlined">Save</Button>
+                    </Card>
+                </Popper>
             </div>
         </Grid>
     )
